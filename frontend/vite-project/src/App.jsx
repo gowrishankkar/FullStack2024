516ec095df0d8947b77df54e90abfe7568ddefe1
shankgo <gowri.shankar@tecnotree.com>
2024-08-10 20:43:04 +0530

    UI integraction done

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,78 +1,57 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
-function loadScript(src) {
-  return new Promise((resolve) => {
-    const script = document.createElement("script");
-    script.src = src;
-    script.onload = () => {
-      resolve(true);
-    };
-    script.onerror = () => {
-      resolve(false);
-    };
-    document.body.appendChild(script);
-  });
-}
+import NavBar from "./components/NavBar";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import PageNotFound from "./pages/PageNotFound";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import User from "./pages/User";
+import PaginationProvider from "./contexts/PaginationContext";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import AuthProvider from "./contexts/AuthProvider";
+import RequireAuth from "./components/RequireAuth";
 
 function App() {
-  const displayRazorpay = async () => {
-    const res = await loadScript(
-      "https://checkout.razorpay.com/v1/checkout.js"
+  const location = useLocation();
+  const [showNavbar, setShowNavbar] = useState(true);
+  useEffect(() => {
+    setShowNavbar(
+      location.pathname !== "/signup" && location.pathname !== "/login"
     );
-    if (!res) {
-      alert("Razorpay SDK failed to load. Are you online?");
-      return;
-    }
-    const responseObj = await fetch("http://localhost:3000/checkout", {
-      method: "POST",
-    });
-    const paymentResponse = await responseObj.json();
-    console.log(paymentResponse);
-    const { id, currency, amount } = paymentResponse.data;
-    const options = {
-      key: "rzp_test_y9KN7luxGUi2Vc", // Enter the Key ID generated from the Dashboard
-      amount: amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-      currency: currency,
-      name: "Acme Corp",
-      description: "Test Transaction",
-      image: "",
-      order_id: id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-      // callback_url: "http://localhost:3000/verify",
-      // notes: {
-      //   address: "Razorpay Corporate Office",
-      // },
-      handler: function (response) {
-        alert(response.razorpay_payment_id);
-        alert(response.razorpay_order_id);
-        alert(response.razorpay_signature);
-      },
-      theme: {
-        color: "#3399cc",
-      },
-    };
-    console.log("options", options);
-    const paymentObject = new window.Razorpay(options);
-    paymentObject.open();
-  };
-
+  }, [location]);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>React Payments</h1>
-      <a onClick={displayRazorpay} target="_blank">
-        Make Payment
-      </a>
-    </>
+    <AuthProvider>
+      <PaginationProvider>
+        {showNavbar && <NavBar></NavBar>}
+        <Routes>
+          <Route path="/" element={<Home></Home>}>
+            {" "}
+          </Route>
+          <Route path="/signup" element={<Signup></Signup>}></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
+          <Route element={<RequireAuth></RequireAuth>}>
+            <Route path="/cart" element={<Cart></Cart>}></Route>
+          </Route>
+          <Route
+            path="/product/:id"
+            element={<ProductDetails></ProductDetails>}
+          >
+            {" "}
+          </Route>
+          <Route path="/user" element={<User></User>}>
+            {" "}
+          </Route>
+          <Route path="/home" element={<Navigate to="/"></Navigate>}></Route>
+          <Route path="*" element={<PageNotFound></PageNotFound>}>
+            {" "}
+          </Route>
+        </Routes>
+      </PaginationProvider>
+    </AuthProvider>
   );
 }
 
--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { Provider } from 'react-redux';
+import store from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
+// import Routing from './poc/Routing.jsx';
+// import Context from './poc/Context.jsx';
+// import ThemeManger from './poc/context/themes/ThemeManger.jsx';
+ReactDOM.createRoot(
+  document.getElementById('root'))
+  .render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        {/* <Routing></Routing> */}
+        {/* <Context></Context> */}
+        {/* <ThemeManger></ThemeManger> */}
+      </BrowserRouter>
+    </Provider>
+    ,
+  )
